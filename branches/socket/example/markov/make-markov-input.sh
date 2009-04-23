b7f3dded35ccf8fd84bb46ab0e0904774afef4ec
higepon <higepon@3dc39933-274d-0410-bf6f-2fa8959f7829>
2009-04-23 08:56:10 +0000

    Added irc-client and markov sample.

--- branches/socket/example/markov/make-markov-input.sh
+++ branches/socket/example/markov/make-markov-input.sh
@@ -3,7 +3,7 @@
 # Text file => markov-input.txt
 #mecab -Owakati < ./data.txt > markov-input.txt
 
-mecab -Owakati < ~/higepon/diary.txt > markov-input.txt
+mecab -Owakati < data.txt > markov-input.txt
 
 # Make dictionary
 ../../mosh markov.ss markov-input.txt
